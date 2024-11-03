f158093c0d871a8839d5ddcdd52d571d4a09479d
DeepAmnesia <81936004+DeepAmnesia@users.noreply.github.com>
2024-11-03 10:34:40 +0300

    Update main.c
    rework

--- main.c
+++ main.c
@@ -3,28 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void ReplaceWord(char* str, char* oldWord, char* newWord);
-void ReadFromFile(const char* path);
-void ReadFromConsole();
-void Replace();
-
-char StringsList[1000][1000];
-int  FilledStrings = 0;
-
-char OldWords[1000][100];
-char NewWords[1000][100];
-int  FilledPairs = 0;
-
-
-int main() {
-    ReadFromFile("words.txt");
-    ReadFromConsole();
-    Replace();
-    return 0;
-}
+#define MAX_STRINGS 1000
+#define STRING_LENGTH 100
 
 void ReplaceWord(char* str, char* oldWord, char* newWord) {
-    char* pos, temp[1000];
+    char* pos, temp[STRING_LENGTH];
     int index = 0;
     int owlen;
 
@@ -39,44 +22,33 @@ void ReplaceWord(char* str, char* oldWord, char* newWord) {
     }
 }
 
-void ReadFromConsole() {
-    char TempString[1000];
+int main() {
+    char OldWords[MAX_STRINGS][STRING_LENGTH];
+    char NewWords[MAX_STRINGS][STRING_LENGTH];
+    int  FilledPairs = 0;
 
-    for (int i = 0; i < 1000; i++) {
-        gets(TempString);
-
-        char* substring_ptr = strstr(TempString, "\\n");
-        if (substring_ptr)
-            break;
-
-        memcpy(StringsList[i], TempString, sizeof(TempString));
-        FilledStrings++;
-    }
-}
-
-void ReadFromFile(const char* path) {
-    FILE* FilePointer = fopen(path, "r");
-
+    FILE* FilePointer = fopen("words.txt", "r");
     char buffer[200];
 
-    while (fgets(buffer, 200, FilePointer)) {
+    while (fgets(buffer, STRING_LENGTH * 2, FilePointer)) {
         char* substring_ptr = strstr(buffer, "\\t");
         if (substring_ptr) {
             memcpy(OldWords[FilledPairs], &buffer[0], &buffer[substring_ptr - buffer] - &buffer[0]);
-            memcpy(NewWords[FilledPairs], &buffer[substring_ptr - buffer + 2], 100);
+            memcpy(NewWords[FilledPairs], &buffer[substring_ptr - buffer + 2], STRING_LENGTH);
             printf("%s | %s\n", OldWords[FilledPairs], NewWords[FilledPairs]);
             FilledPairs++;
         }
     }
 
     fclose(FilePointer);
-}
 
-void Replace() {
-    for (int index = 0; index < FilledStrings; index++) {
-        for (int pair = 0; pair < FilledPairs; pair++) {
-            ReplaceWord(StringsList[index], OldWords[pair], NewWords[pair]);
-        }
-        printf("%s", StringsList[index]);
+    char String[STRING_LENGTH];
+    gets(String);
+
+    for (int pair = 0; pair < FilledPairs; pair++) {
+        ReplaceWord(String, OldWords[pair], NewWords[pair]);
     }
+    printf("%s", String);
+
+    return 0;
 }
